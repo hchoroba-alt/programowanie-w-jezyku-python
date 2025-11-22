bfa74c9204589cc9791cec3f19c50db85da05738
Henryka Choroba <choro@MacBook-Air-2.local>
2025-11-22 14:19:29 +0100

    zadanie 6 dodano

---
+++ zadanie6_potega.py
@@ -0,0 +1,9 @@
+def potega(liczba, wykladnik):
+    wynik = liczba ** wykladnik
+    print(f"{liczba} do potęgi {wykladnik} to: {wynik}")
+
+
+if __name__ == "__main__":
+    potega(2, 3)   # 2^3 = 8
+    potega(5, 2)   # 5^2 = 25
+    potega(10, 0)  # 10^0 = 1
